86494404c6b6cc763c19fae601ef0d54c0f01bf3
Barry <barry_moz@yahoo.com>
2020-09-21 18:43:35 -0400

    more config files

--- web-app/client/src/services/api.js
+++ web-app/client/src/services/api.js
@@ -1,7 +1,16 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default() => {
-  return axios.create({
-    baseURL: `http://localhost:8081`
-  })
-}
+    let apiHost = process.env.VUE_APP_API_HOST;
+    let apiPort = process.env.VUE_APP_API_PORT;
+
+    if (!apiHost){
+        apiHost = "localhost";
+    }
+    if (!apiPort) {
+        apiPort = "8081";
+    }
+    return axios.create({
+        baseURL: "http://" + apiHost + ":" + apiPort
+    });
+};
